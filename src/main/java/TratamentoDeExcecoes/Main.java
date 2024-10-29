a776b50a0617b0b33d9e780694aba6ab68f86d45
FilipeIzidorio <fmotor312@gmail.com>
2024-10-29 20:17:28 -0300

    commit

--- src/main/java/TratamentoDeExcecoes/Main.java
+++ src/main/java/TratamentoDeExcecoes/Main.java
@@ -18,26 +18,26 @@ public class Main {
     public static void main(String[] args) throws ArquivoNaoEscritoException, FileNotFoundException {
         Produto produto1 = new Produto();
         produto1.setCodigo(1032);
-        produto1.setDescricao("CHURRASCO");
+        produto1.setDescricao(" FRANGO");
         produto1.setTipo("9");
-        produto1.setValor(12.0);
+        produto1.setValor(5.00);
 
         Produto produto2 = new Produto();
         produto2.setCodigo(1033);
-        produto2.setDescricao("PICANHA");
+        produto2.setDescricao(" LIGUINCA");
         produto2.setTipo("9");
-        produto2.setValor(15.00);
+        produto2.setValor(5.00);
 
         List<Produto> produtos = new ArrayList<>();
         produtos.add(produto1);
         produtos.add(produto2);
 
-//        IBalanca balancaFilizola = BalancaFactory.getBalanca(TipoBalanca.FINIZOLA_SMART);
-//        balancaFilizola.exportar(produtos, "src/main/java/TratamentoDeExcecoes/testeFilizola.txt");
+        IBalanca balancaFilizola = BalancaFactory.getBalanca(TipoBalanca.FINIZOLA_SMART);
+        balancaFilizola.exportar(produtos, "src/main/java/TratamentoDeExcecoes/testeFilizola.txt");
 
 
-//        IBalanca balancaToledo = BalancaFactory.getBalanca(TipoBalanca.TOLEDO_MGV6);
-//        balancaToledo.exportar(produtos, "src/main/java/TratamentoDeExcecoes/testeToledo.txt");
+        IBalanca balancaToledo = BalancaFactory.getBalanca(TipoBalanca.TOLEDO_MGV6);
+        balancaToledo.exportar(produtos, "src/main/java/TratamentoDeExcecoes/testeToledo.txt");
 
         IBalanca balancaUrano = BalancaFactory.getBalanca(TipoBalanca.URANO_INTEGRA);
         balancaUrano.exportar(produtos, "src/main/java/TratamentoDeExcecoes/testeUrano.txt");
